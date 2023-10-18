4b3721e49c4861737a22397a2f1b3daa6da9642a
szammyboi <78485053+szammyboi@users.noreply.github.com>
2023-10-18 01:52:38 -0400

    Entry

---
+++ premake5.lua
@@ -0,0 +1,54 @@
+---@diagnostic disable: lowercase-global, undefined-global
+
+require "ninja"
+require "export-compile-commands"
+
+workspace "Mythos"
+    configurations {"release", "debug"}
+    targetdir "build"
+    architecture "x86_64"
+    flags "MultiProcessorCompile"
+
+outputdir = "%{cfg.buildcfg}"
+
+include "vendor/glfw-premake"
+
+project "Mythos"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "off"
+
+    targetdir ("build/bin/" .. outputdir)
+    objdir ("build/bin-int/" .. outputdir)
+
+    symbols "on"
+    buildoptions {"-Werror", "-Wuninitialized", "-Wno-narrowing"}
+    files 
+    { 
+        "src/**.h",
+        "src/**.cpp"
+    }
+
+
+    -- Should search for vulkan sdk instead of hardcoding
+    includedirs 
+    {
+        "vendor/glfw-premake/include",
+        "vendor/glm",
+        "C:/Dev/C++/Libraries/VulkanSDK/include"
+    }
+
+    libdirs {
+        "C:/Dev/C++/Libraries/VulkanSDK/lib"
+    }
+
+    links 
+    {
+        "vulkan-1",
+        "GLFW",
+        "gdi32"
+    }
+
+    filter "configurations:debug"
+        defines "DEBUG"
